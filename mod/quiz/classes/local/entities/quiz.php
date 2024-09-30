fc1403cc95af1d4a77b0c5e0fed354eaf5d525fe
Thiago Livramento <tlivramento.adapta@gmail.com>
2024-09-30 17:41:41 -0300

    inicio da entidade quiz para o report builder

--- mod/quiz/classes/local/entities/quiz.php
+++ mod/quiz/classes/local/entities/quiz.php
@@ -14,3 +14,81 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+//declare(strict_types=1); Verificar se é necessário.
+
+namespace mod_quiz\local\entities;
+
+use core_reportbuilder\local\filters\{date, duration, number, text};
+use core_reportbuilder\local\report\{column, filter};
+use core_reportbuilder\local\entities\base;
+use core_reportbuilder\local\helpers\format;
+use lang_string;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/course/lib.php');
+
+/**
+ * Quiz entity class implementation quiz
+ *
+ * This entity defines all the quiz columns and filters to be used in any report.
+ *
+ * @package     mod_quiz
+ * @copyright   TODO
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ class quiz extends base {
+
+     /** @var array */
+     private $acronyms = [];
+
+     /**
+      * Database tables that this entity uses
+      *
+      * @return string[]
+      */
+     protected function get_default_tables(): array {
+         return [
+         //Ver os campos que serão disponibilizados.
+         ];
+     }
+     
+     /**
+      * the default title for this entity
+      *
+      * @return lagn_string
+      */
+
+     protected function get_default_entity_title(): lang_string {
+         return new lang_string('quizreport', 'mod_quiz');
+     }
+
+     /**
+     * Initialise the entity, add all quiz fields
+     *
+     * @return base
+     */
+
+    public function initialise(): base {
+
+        $columns = $this->get_all_columns();
+        foreach ($columns as $column) {
+            $this->add_columns($column);
+        }
+    
+        $filters = $this->get_all_filters();
+        foreach ($filters as $filter) {
+            $this->add_filter($filter);
+        }
+
+        $conditions = $this->get_all_filters();
+        foreach ($conditions as $condition) {
+            $this->add_condition($condition);
+        }
+
+        return $this;
+    }
+
+
